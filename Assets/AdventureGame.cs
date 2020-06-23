5bb0356900e3adf8d7fef780a93a08affa1eb9b5
Andrej <rooney.andrej@gmail.com>
2020-06-23 04:56:21 +0200

    System for Game states

--- Assets/AdventureGame.cs
+++ Assets/AdventureGame.cs
@@ -8,15 +8,43 @@ public class AdventureGame : MonoBehaviour
     [SerializeField]
     Text textComponent;
 
+    [SerializeField]
+    State startingState;
+
+    State state;
+
     // Start is called before the first frame update
     void Start()
     {
-        textComponent.text = ("I am added programmatically");
+        state = startingState;
+        textComponent.text = state.GetStateStory(); 
     }
 
     // Update is called once per frame
     void Update()
     {
+        ManageState();   
+    }
+
+    private void ManageState()
+    {
+        var nextState = state.GetNextState();
         
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            state = nextState[0];
+        }
+
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            state = nextState[1];
+        }
+
+        else if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            state = nextState[2];
+        }
+
+        textComponent.text = state.GetStateStory();
     }
 }
--- Assets/State.cs
+++ Assets/State.cs
@@ -5,8 +5,22 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "State")]
 public class State : ScriptableObject
 {
-    [TextArea(14,10)] [SerializeField]
+    [SerializeField] [TextArea(14, 10)]
     string storyText;
+
+    [SerializeField]
+    State[] nextStates;
+
+    public string GetStateStory()
+    {
+        return storyText;
+    }
+
+    public State[] GetNextState() 
+    {
+        return nextStates;
+    }
+
 }
 
 
